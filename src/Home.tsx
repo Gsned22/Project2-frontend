709658b4e8f356d71a9828582f7015d92bde5e5b
Victor Cizek <victorjcizek@gmail.com>
2023-02-11 18:34:23 -0500

    added books to homepage

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Home() {
-    return (
+
+    const [books, setBooks] = useState<{
+        product_number: string,
+        description: string,
+        image: string,
+        in_stock: boolean,
+        price: number
+        product_name: string }[]>([]);
+
+    useEffect(() => {
+        retrieveBooks();
+    }, [])
+
+    async function retrieveBooks() {
+        const response = await axios.get('http://127.0.0.1:8080/products');
+        setBooks(response.data.Items);
+    }
+    
+    return ( 
         <>
-            <h1>Homepage</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui fugiat quidem praesentium delectus asperiores inventore numquam natus voluptatum tempore veniam!</p>
+            <h1>Welcome to Alchemy Booksellers</h1>
+
+            <h2>Products</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Product Number</th>
+                        <th>Product Name</th>
+                        <th>Description</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {books.map((books) => {
+                        return (
+                            <tr key={books.product_number}>
+                                <td>{books.product_number}</td>
+                                <td>{books.product_name}</td>
+                                <td>{books.description}</td>
+                                <td>{books.price}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
         </>
     )
 }
