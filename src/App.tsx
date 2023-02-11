6fc476e37f74dda9190c73f2fb391d10d3a7babc
Victor Cizek <victorjcizek@gmail.com>
2023-02-11 13:41:56 -0500

    added User Profile routed to upon login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import About from './About';
-import Employee from './User';
-import FinanceManager from './Guest';
+import Employee from './Employee';
+import FinanceManager from './FinanceManager';
 import Home from './Home';
 import Login from './Login';
-import User from './User';
-import Guest from './Guest';
+import UserProfile from './UserProfile';
 
 function App() {
   return (
@@ -28,8 +27,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/user" element={<User />} />
-        <Route path="/guest" element={<Guest />} /> 
+        <Route path="/users/profile" element={<UserProfile />} />
+        <Route path="/employee" element={<Employee />} />
+        <Route path="/financemanager" element={<FinanceManager />} /> 
       </Routes>
     </BrowserRouter>
   );
--- src/FinanceManager.tsx
+++ src/FinanceManager.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function Guest() {
+function FinanceManager() {
 
     const [reimbursements, setReimbursements] = useState<{ 
         reimbursement_id: string, 
@@ -94,4 +94,4 @@ function Guest() {
     )
 }
 
-export default Guest;
+export default FinanceManager;
