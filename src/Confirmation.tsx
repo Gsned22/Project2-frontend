81945a6f74c5e4ed9a8175bad57e9016c82b35fa
Victor Cizek <victorjcizek@gmail.com>
2023-02-16 09:43:35 -0500

    updated Register, Checkout, Confirmation components

--- src/Confirmation.tsx
+++ src/Confirmation.tsx
@@ -4,9 +4,23 @@ import { useNavigate } from 'react-router-dom';
 
 function Confirmation() {
 
+    const navigate = useNavigate();
+
+    function returnToProducts() {  
+        return navigate("/");
+    }
+
+    function goToUserProfile() {  
+        return navigate("/customer/profile");
+    }
+
     return (
     <>
         <h1>Thank you for your order!</h1>
+        <div className='buttonsOnConfirmationPage'>
+                    <button className='returnToProductsButton' onClick={() => { returnToProducts() }}>Shop More</button>
+                    <button className='goToUserProfileButton' onClick={() => { goToUserProfile() }}>Go to Your User Profile</button>
+        </div>
     </>
     )
 }
--- src/Login.tsx
+++ src/Login.tsx
@@ -28,7 +28,7 @@ function Login() {
             const payload: { iat: number, role: string, username: string } = jwtDecode(token); // extracts the payload of the token
             if (payload.role === 'user') {
                 // redirect to /user route
-                return navigate("/customer/profile");
+                return navigate("/");
             } else if (payload.role === 'admin') {
                 // redirect to /guest route
                 return navigate("/admin");
